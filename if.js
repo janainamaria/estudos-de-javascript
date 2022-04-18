5f5bd4487051bb22c7c18156240a51115cc13e1b
janaina <janainamariamdm@gmail.com>
2022-04-18 15:05:48 -0300

    "testando booleans"

--- if.js
+++ if.js
@@ -5,3 +5,24 @@ function soBoaNoticia(nota){
 }
 soBoaNoticia(8.1)
 soBoaNoticia(6.1)
+
+function seForVerdadeEuFalo(valor){
+    if (valor) {
+        console.log('é verdade' + valor)
+    }
+}
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+// variavel vazia
+seForVerdadeEuFalo(undefined)
+// não foi definida nem um valor definido
+seForVerdadeEuFalo(NaN)
+// resultado de operação ou função que não retornou um numero real valido
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo ('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1,2])
+seForVerdadeEuFalo({})
