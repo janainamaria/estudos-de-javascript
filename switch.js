32c3c2978d5253a067641e51bf03b811eb94e9da
janaina <janainamariamdm@gmail.com>
2022-04-19 13:43:38 -0300

    "estudo de switch"

--- switch.js
+++ switch.js
@@ -1,7 +1,28 @@
-const imprimirResultado = function(nota){
+const imprimirResultado = function(nota) {
     switch (Math,floor(nota)) {
-        case 10:
-        case 9: 
+        case 1:
+            break
+        case 0: 
             console.log('quadro de honra')
-    }
+            break
+        default:
+            console.log('eror')
+        }
 }
+imprimirResultado(10)
+/*
+const nota = '10';
+switch (nota) {
+  case '8':
+    console.log('reprovado');
+    break;
+  case '5':
+      console.log('reprovado');
+      break;
+  case '10':
+    console.log('aprovado');
+    break;
+  default:
+    console.log('ninguem passou :(' + nota);
+}
+
