5c87121455961ea4341cb4aaf0fdd5a4461be396
janaina <janainamariamdm@gmail.com>
2022-04-26 13:59:26 -0300

    for in

---
+++ forin.js
@@ -0,0 +1,17 @@
+const notas = [2,5.6,8.6,10,4.5]
+
+for (const i in notas) {
+    console.log(i,notas [1])
+    }
+    //posicoes com os valores dos arrays
+ 
+const pessoa ={
+    nome: 'ana',
+    sobrenome: 'maria',
+    idade: 20,
+    peso: 80
+}
+// criei um objeto
+for (atributo in pessoa) {
+    console.log(`${atributo}=${pessoa[atributo]}`)
+    }
