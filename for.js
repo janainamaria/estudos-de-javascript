a4af7876017201b8fd2fbda28cc07341ac7054db
janaina <janainamariamdm@gmail.com>
2022-04-26 13:33:50 -0300

    for

---
+++ for.js
@@ -0,0 +1,18 @@
+const numeros = [0,1,2,3,4,5]
+for (let index = 0; index < numeros.length; index++) {
+    const element = numeros[index];
+    console.log(element)
+    
+}
+
+
+for (let contador = 1; contador < 100; contador++ ){
+    const element = contador;
+    console.log(element)
+    
+}
+
+const notas = [10,7.5,8,4]
+for (let numero = 0; numero < notas.length; numero++) {
+    console.log(numero)
+}
