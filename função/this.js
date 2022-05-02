b6f007ef36ad832c5f6ebb57c9aabe28bc5291b0
janaina <janainamariamdm@gmail.com>
2022-05-02 18:54:07 -0300

    estudo do this

--- função/this.js
+++ função/this.js
@@ -1,1 +1,8 @@
-// Funções Arrow (seta), arrow functions
+// this pode acessar o atributo de um objeto.]
+const pessoas = {
+    saudacao: 'bom dia',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+pessoas.falar()
