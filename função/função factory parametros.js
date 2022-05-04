d9f37f15b6384a54fad136d228bee7554582f4f6
janaina <janainamariamdm@gmail.com>
2022-05-04 12:10:29 -0300

    atividade factory

--- função/função factory parametros.js
+++ função/função factory parametros.js
@@ -1,9 +1,11 @@
 
 function criarProduto(nome,preço){
     return{
-        nome: nome,
+        nome: nome, //aqui são propriedades
         preço: preço,
         desconto: 45
     }
 }
+//cria quantos objetos quiser
 console.log( criarProduto('janaina',15));
+console.log( criarProduto('anna',1));
